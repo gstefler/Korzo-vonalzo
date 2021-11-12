30cdd20ea832b80ca28c5822151380b5da3749a4
Stefler Gábor <gabor.stefler@gmail.com>
2021-11-12 02:17:30 +0100

    haladas

--- Skeleton.cpp
+++ Skeleton.cpp
@@ -1,5 +1,5 @@
 //=============================================================================================
-// Mintaprogram: Z�ld h�romsz�g. Ervenyes 2019. osztol.
+// Mintaprogram: Zöld háromszög. Ervenyes 2019. osztol.
 //
 // A beadott program csak ebben a fajlban lehet, a fajl 1 byte-os ASCII karaktereket tartalmazhat, BOM kihuzando.
 // Tilos:
@@ -33,121 +33,252 @@
 //=============================================================================================
 #include "framework.h"
 
-// vertex shader in GLSL: It is a Raw string (C++11) since it contains new line characters
 const char * const vertexSource = R"(
-	#version 330				// Shader 3.3
-	precision highp float;		// normal floats, makes no difference on desktop computers
+	#version 330
+	precision highp float;
 
-	uniform mat4 MVP;			// uniform variable, the Model-View-Projection transformation matrix
-	layout(location = 0) in vec2 vp;	// Varying input: vp = vertex position is expected in attrib array 0
+	layout(location = 0) in vec2 vp;
+
+    out vec2 texCoord;
 
 	void main() {
-		gl_Position = vec4(vp.x, vp.y, 0, 1) * MVP;		// transform vp from modeling space to normalized device space
+		gl_Position = vec4(vp.x, vp.y, 0, 1);
+        texCoord = vp * 0.5 + 0.5;
 	}
 )";
 
-// fragment shader in GLSL
+
 const char * const fragmentSource = R"(
-	#version 330			// Shader 3.3
-	precision highp float;	// normal floats, makes no difference on desktop computers
-	
-	uniform vec3 color;		// uniform variable, the color of the primitive
-	out vec4 outColor;		// computed color of the current pixel
+	#version 330
+	precision highp float;
+
+    in vec2 texCoord;
+
+    uniform sampler2D tex;
+
+    out vec4 outColor;
 
 	void main() {
-		outColor = vec4(color, 1);	// computed color is the color of the primitive
+		outColor = texture(tex, texCoord);
 	}
 )";
 
-GPUProgram gpuProgram; // vertex and fragment shaders
-unsigned int vao;	   // virtual world on the GPU
+struct Cube{
+	vec2 pos;
+	float size;
 
-// Initialization, create an OpenGL context
+	Cube(vec2 pos, float size) : pos(pos), size(size) {}
+
+	bool isInside(vec2 point){
+		return (point.x >= pos.x - size && point.x <= pos.x + size && point.y >= pos.y - size && point.y <= pos.y + size);
+	}
+
+	vec4 getColor(vec2 point) {
+		if (isInside(point)) {
+			return vec4(1.0f, 1.0f, 0.0f, 1.0f);
+		}
+		return vec4(0.0f, 0.0f, 0.0f, 1.0f);
+	}
+};
+
+
+struct Circle{
+	vec2 center;
+	float radius;
+	float thickness = 4.0f;
+
+	Circle(vec2 center, float radius) : center(center), radius(radius) {}
+
+	bool isInside(vec2 point) {
+		return length(center - point) < radius && length(center - point) > radius - thickness;
+	}
+
+	vec4 getColor(vec2 point, vec4 color = vec4(1.0f, 0.0f, 0.0f, 1.0f)) {
+		if (isInside(point)) {
+			return color;
+		}
+		else {
+			return vec4(0.0f, 0.0f, 0.0f, 1.0f);
+		}
+	}
+
+	// retuns 2 points that are the intersection of an other circle
+	std::vector<vec2> getIntersectionPoints(Circle other) {
+		std::vector<vec2> points;
+		float d = length(center - other.center); 
+		float a = (radius + other.radius) * (radius + other.radius);
+		float b = 2 * (other.radius * (other.radius - radius) + radius * (radius - other.radius));
+		float c = (radius - other.radius) * (radius - other.radius);
+		float x1 = (d * d + a - b) / (2 * d);
+		float x2 = (d * d - a + b) / (2 * d);
+		float y1 = sqrt(c - x1 * x1);
+		float y2 = sqrt(c - x2 * x2);
+		points.push_back(vec2(x1, y1) + center);
+		points.push_back(vec2(x2, y2) + center);
+		return points;
+	}
+};
+
+struct Line{
+	vec2 start;
+	vec2 end;
+	float thickness = 4.0f;
+
+	Line(vec2 start, vec2 end) : start(start), end(end) {}
+
+	bool isInside(vec2 point) {
+		vec2 v = end - start;
+		vec2 w = point - start;
+		float c1 = dot(w, v);
+		if (c1 <= 0) return true;
+		float c2 = dot(v, v);
+		if (c2 <= c1) return true;
+		return false;
+	}
+
+	vec4 getColor(vec2 point, vec4 color = vec4(1.0f, 0.0f, 0.0f, 1.0f)) {
+		if (isInside(point)) {
+			return color;
+		}
+		else {
+			return vec4(0.0f, 0.0f, 0.0f, 1.0f);
+		}
+	}
+};
+
+
+vec2 start;
+vec2 end;
+
+
+bool isSpressed = false;
+bool isCpressed = false;
+bool isIpressed = false;
+bool isLpressed = false;
+
+bool isMousePressed = false;
+
+GPUProgram gpuProgram;
+unsigned int vao;
+
+// ebben tároljuk a megjelenitendo objektumokat
+Texture texture;
+
+std::vector<Circle> circles;
+std::vector<Line> lines;
+
 void onInitialization() {
 	glViewport(0, 0, windowWidth, windowHeight);
 
-	glGenVertexArrays(1, &vao);	// get 1 vao id
-	glBindVertexArray(vao);		// make it active
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 
-	unsigned int vbo;		// vertex buffer object
-	glGenBuffers(1, &vbo);	// Generate 1 buffer
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	// Geometry with 24 bytes (6 floats or 3 x 2 coordinates)
-	float vertices[] = { -0.8f, -0.8f, -0.6f, 1.0f, 0.8f, -0.2f };
-	glBufferData(GL_ARRAY_BUFFER, 	// Copy to GPU target
-		sizeof(vertices),  // # bytes
-		vertices,	      	// address
-		GL_STATIC_DRAW);	// we do not change later
 
-	glEnableVertexAttribArray(0);  // AttribArray 0
-	glVertexAttribPointer(0,       // vbo -> AttribArray 0
-		2, GL_FLOAT, GL_FALSE, // two floats/attrib, not fixed-point
-		0, NULL); 		     // stride, offset: tightly packed
+    // a 2ds lap amire rajzolunk
+	float vertices[] = { -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f };
+
+    glBufferData(GL_ARRAY_BUFFER,
+		sizeof(vertices),
+		vertices,
+		GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0,
+		2, GL_FLOAT, GL_FALSE,
+		0, NULL);
 
 	// create program for the GPU
 	gpuProgram.create(vertexSource, fragmentSource, "outColor");
 }
 
-// Window has become invalid: Redraw
+
 void onDisplay() {
-	glClearColor(0, 0, 0, 0);     // background color
-	glClear(GL_COLOR_BUFFER_BIT); // clear frame buffer
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
 
-	// Set color to (0, 1, 0) = green
 	int location = glGetUniformLocation(gpuProgram.getId(), "color");
-	glUniform3f(location, 0.0f, 1.0f, 0.0f); // 3 floats
 
-	float MVPtransf[4][4] = { 1, 0, 0, 0,    // MVP matrix, 
-							  0, 1, 0, 0,    // row-major!
-							  0, 0, 1, 0,
-							  0, 0, 0, 1 };
+    std::vector<vec4> pixels;
 
-	location = glGetUniformLocation(gpuProgram.getId(), "MVP");	// Get the GPU location of uniform variable MVP
-	glUniformMatrix4fv(location, 1, GL_TRUE, &MVPtransf[0][0]);	// Load a 4x4 row-major float matrix to the specified location
 
-	glBindVertexArray(vao);  // Draw call
-	glDrawArrays(GL_TRIANGLES, 0 /*startIdx*/, 3 /*# Elements*/);
+	for (int y = 0; y < windowHeight; y++) {
+		for (int x = 0; x < windowWidth; x++) {
+			
+		}
+	}
 
-	glutSwapBuffers(); // exchange buffers for double buffering
+			
+
+    texture.create(windowWidth, windowHeight, pixels);
+    gpuProgram.setUniform(texture, "tex");
+
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+	glutSwapBuffers();
 }
+
 
 // Key of ASCII code pressed
 void onKeyboard(unsigned char key, int pX, int pY) {
-	if (key == 'd') glutPostRedisplay();         // if d, invalidate display, i.e. redraw
+	switch (key) {
+	case 's':
+		isSpressed = true;
+		break;
+	case 'c':
+		isCpressed = true;
+		break;
+	case 'i':
+		isIpressed = true;
+		break;
+	case 'l':
+		isLpressed = true;
+		break;
+	}
 }
+
 
 // Key of ASCII code released
 void onKeyboardUp(unsigned char key, int pX, int pY) {
+	switch (key) {
+	case 's':
+		isSpressed = false;
+		break;
+	case 'c':
+		isCpressed = false;
+		break;
+	case 'i':
+		isIpressed = false;
+		break;
+	case 'l':
+		isLpressed = false;
+		break;
+	}
 }
+
 
 // Move mouse with key pressed
 void onMouseMotion(int pX, int pY) {	// pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
-	// Convert to normalized device space
-	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
-	float cY = 1.0f - 2.0f * pY / windowHeight;
-	printf("Mouse moved to (%3.2f, %3.2f)\n", cX, cY);
+
 }
+
 
 // Mouse click event
 void onMouse(int button, int state, int pX, int pY) { // pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
-	// Convert to normalized device space
-	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
-	float cY = 1.0f - 2.0f * pY / windowHeight;
-
-	char * buttonStat;
-	switch (state) {
-	case GLUT_DOWN: buttonStat = "pressed"; break;
-	case GLUT_UP:   buttonStat = "released"; break;
+    pY = windowHeight - pY;
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		isMousePressed = true;
+		start = vec2(pX, pY);
 	}
 
-	switch (button) {
-	case GLUT_LEFT_BUTTON:   printf("Left button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);   break;
-	case GLUT_MIDDLE_BUTTON: printf("Middle button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY); break;
-	case GLUT_RIGHT_BUTTON:  printf("Right button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);  break;
-	}
+	
+
 }
+
 
 // Idle event indicating that some time elapsed: do animation here
 void onIdle() {
-	long time = glutGet(GLUT_ELAPSED_TIME); // elapsed time since the start of the program
 }
